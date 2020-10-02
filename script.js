f2395e38aeabf26634f45e2f9fd190a538443eaa
geektechstuff <geek@geektechstuff.com>
2020-10-02 14:16:07 +0100

    html, css and js updated to requirements

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    principal = document.getElementById("principal").value;
+
+    // warn if principal 0 or a negative number
+    if (principal <=0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
+    else {
+    rate = document.getElementById("rate").value;
+    // parseInt turns string into integer
+    var years = parseInt(document.getElementById("years").value);
+    // works out interest
+    interest = principal*years*rate/100;
+    // gets todays date
+    var today = new Date();
+    // gets current year from todays date, and changes into integer
+    var year = parseInt(today.getFullYear());
+    var future_year = year+years;
+
+    // returns HTML to the result
+    document.getElementById("result").innerHTML =
+    //<mark> highlights
+    "If you deposit: <mark>" + principal + "</mark><br>\
+    at an interest rate of: <mark>" + rate + "%</mark><br>\
+    You will receive an amount of <mark>" + interest + "</mark><br>\
+    in the year: <mark>" + future_year + "</mark><br>";
+
+    }
     
 }
         
+function slider() {
+    //updates slider rate
+    document.getElementById("result_slider").innerHTML = document.getElementById("rate").value + "%";
+}
